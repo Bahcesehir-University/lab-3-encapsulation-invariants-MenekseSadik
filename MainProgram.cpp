e58535eb9d82f7aa6a305dd85b88dede502bf6e7
MenekseSadik <menekse.sadik@bahcesehir.edu.tr>
2026-03-24 13:41:01 +0300

    Update exception handling in transfer method
    Changed exception type from runtime_error to invalid_argument for negative transfer amounts.

--- MainProgram.cpp
+++ MainProgram.cpp
@@ -151,11 +151,9 @@ public:
     void transfer(BankAccount& other, double amount) {
         
           if(amount <= 0){
-         throw runtime_error("transfer amount must be positive");
+         throw invalid_argument("transfer amount must be positive");
         }
-         if(amount > balance_){
-         throw runtime_error("insufficient funds");
-         }
+         
          withdraw(amount);
          other.deposit(amount);
         
